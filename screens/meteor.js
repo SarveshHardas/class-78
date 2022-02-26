e0e93b61e20528836bab63c94da68024110b307c
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-02-26 20:58:29 +0530

    Add files via upload

---
+++ screens/meteor.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Meteor extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Meteor Location</Text>
+      </View>
+    )
+  }
+}
